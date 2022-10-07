4d1011dd863da2c72ccd384e160174f804dcdcfa
Arun M S <narmusgaming@gmail.com>
2022-10-07 20:56:00 +0530

    Inline styling

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  fontSize: "100px",
+  border: "1px solid black",
+};
 root.render(
   <d>
-    <h1 className="heading">Foods List</h1>
-    <ul>
-      <li>Bacon </li>
-      <li>Cheese </li>
-    </ul>
+    <h1 style={customStyle}>Foods List</h1>
   </d>
 );
