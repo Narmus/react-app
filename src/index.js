b027f3542740cd5f7b330dd5002ee1abb0aabd50
Arun M S <narmusgaming@gmail.com>
2022-10-07 20:47:52 +0530

    CSS External Styling

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hellow World!</h1>);
+root.render(
+  <d>
+    <h1 className="heading">Foods List</h1>
+    <ul>
+      <li>Bacon </li>
+      <li>Cheese </li>
+    </ul>
+  </d>
+);
